18ae04f0c141cf577c33d4e4b7e6f72acfc3514d
Layo Folaranmi <layo@Layos-MacBook-Pro-2.local>
2023-04-28 17:47:10 +0100

    implemented base grpc server

---
+++ server/src/proto/loader.ts
@@ -0,0 +1,24 @@
+import * as path from "path";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader"
+
+let MESSAGE_PROTO_PATH = 'messages.proto';
+let AUTH_PROTO_PATH = 'auth.proto';
+
+const protoOptions: protoLoader.Options = {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true,
+	includeDirs: [ path.join(__dirname, '../../../', 'proto') ]
+}
+
+let _messagePackageDef = protoLoader.loadSync(MESSAGE_PROTO_PATH, protoOptions);
+let _authPackageDef = protoLoader.loadSync(AUTH_PROTO_PATH, protoOptions);
+
+const _messageProtoDescriptor = grpc.loadPackageDefinition(_messagePackageDef);
+const _authProtoDescriptor = grpc.loadPackageDefinition(_authPackageDef);
+
+export const _messages = _messageProtoDescriptor._messages;
+export const _auth = _authProtoDescriptor._auth;
