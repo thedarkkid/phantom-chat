a23740faadd2388b126613d3a81ecf3c3af146b3
Layo Folaranmi <layo@Layos-MacBook-Pro-2.local>
2023-05-05 18:03:28 +0100

    installed and setup sequelize with sqlite

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,21 @@
 import * as grpc from "@grpc/grpc-js";
 import { server } from "./proto/server";
+import { sequelize } from "./db";
 
 server.bindAsync(
   "0.0.0.0:9090",
   grpc.ServerCredentials.createInsecure(),
-  (error, port) => {
-    server.start();
+  async (error, port) => {
     if (error) console.log(error, "PORT::", port);
-    console.info("server started");
+
+    server.start();
+    console.info(`grpc server started on port ${port}`);
+
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (e) {
+      console.error("Unable to connect to the database:", e);
+    }
   }
 );
