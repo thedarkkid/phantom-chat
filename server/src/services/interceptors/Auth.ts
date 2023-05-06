42bc265fdea79506295b0543b53cab9a0a97c400
Layo Folaranmi <thedarkkid.codes@gmail.com>
2023-05-06 20:58:06 +0100

    created auth interceptor
    interceptor uses jwt to validate users

---
+++ server/src/services/interceptors/Auth.ts
@@ -0,0 +1,18 @@
+import Config from "../../common/Config";
+import JWT from "jsonwebtoken";
+import { ServerInterceptor } from "../../proto/interceptor";
+import * as grpc from "@grpc/grpc-js";
+const Auth: ServerInterceptor = (call, callback, next) => {
+  try {
+    const token = call.request.metadata.token;
+    call.request.authorization = JWT.verify(token, Config("JWT_SECRET"));
+    next();
+  } catch (e) {
+    callback({
+      code: grpc.status.UNAUTHENTICATED,
+      message: "Invalid Token",
+    } as any);
+  }
+};
+
+export default Auth;
